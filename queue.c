0c384af15703f9be1001c5af5d2afb33b6f20c32
PhalanxHead <lhedt@hotmail.com>
2017-03-24 17:54:34 +1100

    Update, created frames for stack and queue

---
+++ queue.c
@@ -0,0 +1,17 @@
+/*  Author:     Luke Hedt
+    Date:       00/00/2016
+    Name:       
+    Purpose:    
+
+    Comments:
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    printf("This is a frame program!");
+
+    return 0;
+}
---
+++ stack.c
@@ -0,0 +1,18 @@
+/*  Author:     Luke Hedt
+    StuID:      832153
+    Date:       24/03/2017
+    Name:       stack.c
+    Purpose:    Contains code for Stack Operations
+
+    Comments:
+*/
+
+#include "list.h"
+
+list_t* new_queue()
+/*
+ Creates a new queue object and returns a pointer to it. 
+*/
+{
+    
+}
